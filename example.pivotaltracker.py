b9a696c9478c195908cd45f4ea01bb13f62541ed
Paul Traylor <paul@wegame.com>
2010-01-10 00:10:11 -0800

    Example script to import tasks from Pivotal Tracker to The Hit List

---
+++ example.pivotaltracker.py
@@ -0,0 +1,47 @@
+import sys
+
+if len(sys.argv) != 6:
+	exit('Usage: '+sys.argv[0]+' <tracker_name> <tracker_pass> <tracker_proj_id> <tracker_query> <thl_target>')
+
+try:
+	from pytracker import Tracker
+	from pytracker import Story
+	from pytracker import HostedTrackerAuth
+except ImportError:
+	exit('Fatal Error: Requires pytracker module. http://code.google.com/p/pytracker/')
+
+import TheHitList
+
+tracker_name = sys.argv[1]
+tracker_pass = sys.argv[2]
+tracker_proj = int(sys.argv[3])
+tracker_query = sys.argv[4]
+thl_target = sys.argv[5]
+
+def add_story(story,list):
+	'''
+	Add a PT Story to a THL List
+	@param story: PT Story
+	@param list: THL List
+	'''
+	for task in list.tasks():
+		if task.title.startswith('(%s)'%story.story_id):
+			print 'found', task.title
+			return
+	print 'adding', task.title
+	newtask = TheHitList.Task()
+	newtask.title = '(%s) %s'%(story.story_id,story.name)
+	newtask.notes = story.description
+	list.add_task(newtask)
+
+#Talk to Tracker
+auth = HostedTrackerAuth(tracker_name, tracker_pass)
+tracker = Tracker(tracker_proj, auth)
+stories = tracker.GetStories(tracker_query)
+
+#Talk to THL
+thl = TheHitList.Application()
+list =  thl.find_list(thl_target)
+if list:
+	for story in stories:
+		add_story(story,list)
