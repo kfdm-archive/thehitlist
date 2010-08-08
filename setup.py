a6af47791ad218b8d2af2695cf501892410e6969
Paul Traylor <paul@wegame.com>
2010-08-07 23:00:22 -0700

    Add in setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(
+	name='TheHitList',
+	description='Python library that wraps The Hit List\'s AppleScript api using appscript',
+	author='Paul Traylor',
+	url='http://github.com/kfdm/thehitlist/',
+	version='0.1',
+	py_modules=['TheHitList'],
+	)
