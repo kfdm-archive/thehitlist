379bec30964d34cde01b3a3cd9875efdaad5fc41
Paul Traylor <paul@wegame.com>
2010-08-16 10:41:19 -0700

    Add support for adding a task to a specific list Add support for listing all tasks in a specific list

--- create_task.py
+++ create_task.py
@@ -4,13 +4,18 @@ from optparse import OptionParser
 
 if __name__ == '__main__':
 	parser = OptionParser()
-	parser.add_option("--list", dest="list", help="List tasks in Inbox", default=False,action="store_true")
+	parser.add_option("--show", dest="show", help="Show tasks in a list", default=None)
+	parser.add_option("--list", dest="list", help="Add task to a specific list", default=None)
 	(opts,args) = parser.parse_args()
 	
 	thl = TheHitList.Application()
 	
-	if(opts.list):
-		for task in thl.inbox().tasks():
+	if(opts.show):
+		if opts.show == 'inbox':
+			list = thl.inbox()
+		else:
+			list = thl.find_list(opts.show)
+		for task in list.tasks():
 			print task.title
 		exit()
 	
@@ -18,6 +23,10 @@ if __name__ == '__main__':
 	
 	newtask = TheHitList.Task()
 	newtask.title = ' '.join(args)
-	thl.inbox().add_task(newtask)
+	if opts.list is None:
+		list = thl.inbox()
+	else:
+		list = thl.find_list(opts.list)
+	list.add_task(newtask)
 	print 'Task (%s) has been added'%newtask.title
 		
