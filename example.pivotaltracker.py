1492563e67fa2d57c336488aee3324a8b99ccc05
Paul Traylor <paul@wegame.com>
2011-07-13 20:13:37 -0700

    Make the Pivotal Tracker simpler and remove dependency on my other module

--- example.pivotaltracker.py
+++ example.pivotaltracker.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
-#import getpass
+# Example for importing PivotalTracker stories into THL
 
-#defaults write com.pivotaltracker name <name>
-#defaults write com.pivotaltracker pass <pass>
-#defaults write com.pivotaltracker project <project_id>
-#defaults write com.pivotaltracker query <query>
-#defaults write com.pivotaltracker thl_list <The Hit List list name>
-
+import commands
+import TheHitList
 try:
 	from pytracker import Tracker
 	from pytracker import Story
@@ -14,15 +10,16 @@ try:
 except ImportError:
 	exit('Fatal Error: Requires pytracker module. http://code.google.com/p/pytracker/')
 
-try:
-	import pydefaults
-except ImportError:
-	exit('Fatal Error: Requires pydefaults module. https://github.com/kfdm/pydefaults/')
+# Read in configuration information from git's configuration files
+# To set, from the shell you can use
+# git config <key name> <value>
+# git config thl.list tasks
+PT_LOGIN = commands.getoutput('/usr/local/bin/git config --get pivotal.login')
+PT_PASS = commands.getoutput('/usr/local/bin/git config --get pivotal.password')
+PT_PROJECT_ID = commands.getoutput('/usr/local/bin/git config --get pivotal.projectid')
+PT_QUERY = commands.getoutput('/usr/local/bin/git config --get pivotal.query')
+THL_LIST = commands.getoutput('/usr/local/bin/git config --get thl.list')
 
-settings = pydefaults.database('com.pivotaltracker')
-
-
-import TheHitList
 from extra.Terminal import TerminalController
 term = TerminalController()
 def found(string): print term.render('${BOLD}${RED}Found${NORMAL} %s'%string)
@@ -45,13 +42,13 @@ def add_story(story,list):
 	list.add_task(newtask)
 
 #Talk to Tracker
-auth = HostedTrackerAuth(settings['login'], settings['pass'])
-tracker = Tracker(int(settings['project_id']), auth)
-stories = tracker.GetStories(settings['query'])
+auth = HostedTrackerAuth(PT_LOGIN, PT_PASS)
+tracker = Tracker(int(PT_PROJECT_ID), auth)
+stories = tracker.GetStories(PT_QUERY)
 
 #Talk to THL
 thl = TheHitList.Application()
-list =  thl.find_list(settings['thl_list'].encode('utf-8'))
+list =  thl.find_list(THL_LIST)
 print '' #Line spacer
 if list:
 	for story in stories:
