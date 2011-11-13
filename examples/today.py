30565127abfe728b6c080541ad0dfba12a18d62e
Paul Traylor <kungfudiscomonkey@gmail.com>
2011-11-13 09:41:30 -0800

    Switch today example to use clint

--- examples/today.py
+++ examples/today.py
@@ -1,45 +1,35 @@
 #!/usr/bin/env python
 import datetime
 import TheHitList
-from extra.Terminal import TerminalController
-
-SPACER_STR		= '${BOLD}${BLUE}%s${NORMAL}'
-
-TITLE_STR		= '${BOLD}${WHITE}%s${NORMAL}'
-COMPLETE_STR	= '${BOLD}${CYAN}%s${NORMAL}'
-CANCELED_STR	= '${BOLD}${BLACK}%s${NORMAL}'
-
-DATE_STR		= '${YELLOW}%s${NORMAL}'
-OVERDUE_STR		= '${RED}%s${NORMAL}'
+from clint.textui import puts, colored
 
 thl = TheHitList.Application()
-term = TerminalController()
-spacer = term.render(SPACER_STR%'-'*50)
+spacer = colored.blue('-'*50)
 
 print 'Today\'s Tasks'
 print spacer
 for task in thl.today().tasks():
 	if task.completed:
-		print term.render(COMPLETE_STR % task.title)
+		puts(colored.cyan(task.title))
 	elif task.canceled:
-		print term.render(CANCELED_STR % task.title)
+		puts(colored.black(task.title))
 	else:
-		print term.render(TITLE_STR % task.title)
+		puts(colored.white(task.title))
 	
 	if( task.start_date is not None ):
 		buffer = 'Start: %s'%task.start_date.date()
 		buffer += ' '*( 20 - len(buffer))
-		buffer = term.render(DATE_STR % buffer)
+		buffer = colored.yellow(buffer)
 	else:
 		buffer = ' '*20
 	
 	if( task.due_date is not None ):
 		str = 'Due: %s'%task.due_date.date()
 		if (task.due_date < datetime.datetime.today() ):
-			buffer += term.render(OVERDUE_STR % str)
+			buffer += colored.red(str)
 		else:
-			buffer += term.render(DATE_STR % str)
-	print buffer
+			buffer += colored.yellow(str)
+	puts(buffer)
 	
 	if( task.notes != '' ): print 'Notes: %s'%task.notes
 	
