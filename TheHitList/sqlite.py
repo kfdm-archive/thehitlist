b66ecbc338bb1a80badcc375ef5453a0fc463770
Paul Traylor <kungfudiscomonkey@gmail.com>
2012-07-12 20:46:22 -0700

    Simple draft of reading the Inbox

--- TheHitList/sqlite.py
+++ TheHitList/sqlite.py
@@ -18,6 +18,9 @@ class Application(object):
         self.conn = sqlite3.connect(SQL_FILE)
         self.c = self.conn.cursor()
 
+    def inbox(self):
+        return List(self.c, 'Inbox')
+
     def test(self):
         SQL_STATEMENT = '''
             SELECT ZTITLE, ZSTARTDATE, ZDUEDATE FROM ZTASK
@@ -30,6 +33,38 @@ class Application(object):
             print row[0].encode('utf8', 'replace')
 
 
+class List(object):
+    def __init__(self, cursor, name):
+        self.name = name
+        self.c = cursor
+
+        SQL_STATEMENT = '''
+        SELECT Z_PK, ZPARENTGROUP, ZTITLE, ZTYPE
+        FROM ZGROUP
+        WHERE ZTITLE = '%(title)s'
+        ''' % {'title': name}
+
+        for row in self.c.execute(SQL_STATEMENT):
+            self.id = row[0]
+            self.parent = row[1]
+            self.name = row[2]
+            self.type = row[3]
+
+    def __repl__(self):
+        return '<%(id)s:%(title)s>' % (self.id, self.title)
+
+    def tasks(self):
+        SQL = '''
+        SELECT ZTITLE FROM ZTASK WHERE ZPARENTLIST = %d
+        ''' % (self.id)
+        print SQL
+
+        for row in self.c.execute(SQL):
+            yield {'title': row[0]}
+
+
 if __name__ == '__main__':
     thl = Application()
-    thl.test()
+    print thl.inbox().__dict__
+    for task in thl.inbox().tasks():
+        print task
